9bff202e2f8bf1f7701596ab04920a1d1f0633e1
nglaeser <nglaeser@users.noreply.github.com>
2022-04-18 21:26:32 -0400

    implement basic ops for usig primitives

--- src/usig.rs
+++ src/usig.rs
@@ -5,7 +5,7 @@ use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
 use curve25519_dalek::scalar::Scalar;
 use starsig::TranscriptProtocol;
 use merlin::Transcript;
-use std::ops::Add;
+use std::ops::{Mul,Add};
 
 pub struct Starsig;
 pub trait Sig<SK, VK, S, E> {
@@ -25,6 +25,13 @@ impl SecretKey {
         SecretKey{ scalar }
     }
 }
+impl Add<Update<Scalar>> for SecretKey {
+    type Output = SecretKey;
+    fn add(self, up: Update<Scalar>) -> SecretKey {
+        SecretKey { scalar: self.scalar + up.scalar }
+    }
+}
+
 impl Sig<SecretKey, VerificationKey, Signature, StarsigError> for Starsig {
     fn kgen(&self) -> (SecretKey, VerificationKey) {
         let mut csprng = OsRng{};
@@ -40,10 +47,21 @@ impl Sig<SecretKey, VerificationKey, Signature, StarsigError> for Starsig {
     }
 }
 
-// TODO impl Mul
 #[derive(Copy,Clone)]
 pub struct Update<T> where T: Copy + Clone {
-    pub up: T,
+    pub scalar: T,
+}
+impl Mul<RistrettoPoint> for Update<Scalar> {
+    type Output = RistrettoPoint;
+    fn mul(self, point: RistrettoPoint) -> RistrettoPoint {
+        self.scalar * point
+    }
+}
+impl Mul<Update<Scalar>> for Scalar {
+    type Output = Scalar;
+    fn mul(self, up: Update<Scalar>) -> Scalar {
+        self * up.scalar
+    }
 }
 pub trait Updatable<SK, VK, S, T: Copy + Clone> {
     // sk_up := sk `op` up_sk
@@ -55,23 +73,24 @@ pub trait Updatable<SK, VK, S, T: Copy + Clone> {
 // impl Updatable<SecretKey, VerificationKey, Signature, Scalar> for dyn Sig<SecretKey, VerificationKey, Signature, StarsigError> {
 impl Updatable<SecretKey, VerificationKey, Signature, Scalar> for Starsig {
     fn upk(&self, pk: VerificationKey) -> (VerificationKey, Update<Scalar>) {
-        // in the case of starsig, mu(`op`) b is + b * RISTRETTO_BASEPOINT_POINT
-        // pk_up  = (sk * RISTRETTO_BASEPOINT_POINT) + (up_sk * RISTRETTO_BASEPOINT_POINT)
+        // mu(`op`) b is + b * RISTRETTO_BASEPOINT_POINT
+        // pk_up := (sk * RISTRETTO_BASEPOINT_POINT) + (up_sk * RISTRETTO_BASEPOINT_POINT)
         //        = (sk + up_sk) * RISTRETTO_BASEPOINT_POINT
         //        = sk_up * RISTRETTO_BASEPOINT_POINT
         let mut csprng = OsRng{};
         let r = Scalar::random(&mut csprng);
-        let up = Update{ up: r };
+        let up = Update{ scalar: r };
 
         let pk_compressed: CompressedRistretto = pk.into();
         let pk_point: RistrettoPoint = pk_compressed.decompress().unwrap();
-        let pk_up: VerificationKey = VerificationKey::from(pk_point + (up.up*RISTRETTO_BASEPOINT_POINT));
+        let pk_up: VerificationKey = VerificationKey::from(pk_point + (up*RISTRETTO_BASEPOINT_POINT));
 
         (pk_up, up)
     }
     fn usk(&self, sk: SecretKey, up: Update<Scalar>) -> SecretKey {
-        // in the case of starsig, `op` is +
-        SecretKey{ scalar: sk.scalar.add(up.up) } // TODO impl Add
+        // `op` is +
+        // sk_up := sk + up_sk
+        sk + up
     }
     fn usig(&self, m: &'static [u8], sigma: Signature, up: Update<Scalar>) -> Signature {
         // sigma_up := sigma + c * up_sk 
@@ -82,8 +101,6 @@ impl Updatable<SecretKey, VerificationKey, Signature, Scalar> for Starsig {
             transcript.append_point(b"R", &sigma.R);
             transcript.challenge_scalar(b"c")
         };
-        let s_new = sigma.s + c * up.up;
-
-        Signature { s: s_new, R: sigma.R }
+        sigma + c * up
     }
 }
